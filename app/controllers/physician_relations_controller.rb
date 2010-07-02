e5f53141a992ca901ab324e8a575cd1db9d91fa0
Sam Grigg <griggsn@ldschurch.org>
2010-07-02 00:08:36 -0600

    Took all the product pages through some final fit 'n' finish. Now all that's left for the roduct pages is to get the video to play in the popup

--- app/controllers/physician_relations_controller.rb
+++ app/controllers/physician_relations_controller.rb
@@ -1,7 +1,7 @@
 class PhysicianRelationsController < ApplicationController
   def index
     if session["current_username"]
-      @demo_url = "http://marketware.com.s3.amazonaws.com/Videos/physician_relations.flv"
+      @demo_url = "http://marketware.com.s3.amazonaws.com/Videos/mw-prm-demo.mp4"
       @trial_url = "http://marketware.com.s3.amazonaws.com/files/marketware_setup.exe"
     else
       @demo_url = "#user-form-box"
