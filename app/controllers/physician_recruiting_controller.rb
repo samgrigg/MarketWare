bb57b500f19fa32530426f0519eb1f8cfbe38a40
Sam Grigg <griggsn@ldschurch.org>
2010-07-14 03:03:46 -0600

    Propagated changes through the product pages

--- app/controllers/physician_recruiting_controller.rb
+++ app/controllers/physician_recruiting_controller.rb
@@ -1,5 +1,23 @@
 class PhysicianRecruitingController < ApplicationController
   def index
+    @demo_url = {
+      :mp4 => "http://marketware.com.s3.amazonaws.com/videos/mw-prm-demo.mp4",
+      :oog => "http://marketware.com.s3.amazonaws.com/videos/mw-prm-demo.oog"
+    }
+    if session["current_username"]
+      @trial_url = "http://marketware.com.s3.amazonaws.com/files/marketware_setup.exe"
+    else
+      @trial_url = "#user-form-box"
+    end
+  end
+  
+  def customize
+  end
+  
+  def reporting
+  end
+  
+  def tech_specs
   end
 
 end
--- app/controllers/physician_relations_controller.rb
+++ app/controllers/physician_relations_controller.rb
@@ -1,8 +1,8 @@
 class PhysicianRelationsController < ApplicationController
   def index
     @demo_url = {
-      :mp4 => "http://marketware.com.s3.amazonaws.com/Videos/mw-prm-demo.mp4",
-      :oog => "http://marketware.com.s3.amazonaws.com/Videos/mw-prm-demo.oog"
+      :mp4 => "http://marketware.com.s3.amazonaws.com/videos/mw-prm-demo.mp4",
+      :oog => "http://marketware.com.s3.amazonaws.com/videos/mw-prm-demo.oog"
     }
     if session["current_username"]
       @trial_url = "http://marketware.com.s3.amazonaws.com/files/marketware_setup.exe"
