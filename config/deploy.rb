09590d9fce1d7fdcc95453d604051844f136602b
Sam Grigg <griggsn@ldschurch.org>
2010-08-12 23:50:43 -0600

    some little deployment stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :scm, :git
 role :web, "marketwarecrm.com"                          # Your HTTP server, Apache/etc
 role :app, "marketwarecrm.com"                          # This may be the same as your `Web` server
 role :db,  "marketwarecrm.com", :primary => true # This is where Rails migrations will run
-set :deploy_to, "/mktwarecrm.com/apps/marketwarecrm"
+set :deploy_to, "home/mktwarecrm.com/apps/marketwarecrm"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
