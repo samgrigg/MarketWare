16b4f478c0fcb8c9e1af8e747b85f43ce7685272
Sam Grigg <griggsn@ldschurch.org>
2010-08-18 01:59:46 -0600

    email settings

--- config/environment.rb
+++ config/environment.rb
@@ -44,7 +44,7 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 
-	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.delivery_method = :sendmail
 	config.action_mailer.smtp_settings = {
 	  :address => 'mail.marketwarecrm.com',
 	  :port => 587,
@@ -53,7 +53,7 @@ Rails::Initializer.run do |config|
 	  :user_name => 'marketware_admin@marketwarecrm.com',
 	  :password => 'Pas-word0'
 	}
-	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.default_charset = 'utf-8'
 end
