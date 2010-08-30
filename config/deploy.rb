fcd146c77f3307dbb4f1a5419ab275c2caa63558
Sam Grigg <griggsn@ldschurch.org>
2010-08-30 16:33:32 -0600

    Made /maps symlink and forced IE 8 to standards mode

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,9 @@ namespace :deploy do
 		run "ln -nfs #{shared_path}/system/files/marketware10_setup.exe #{release_path}/public/marketware10_setup.exe"
 		run "ln -nfs #{shared_path}/system/files/last_update.txt #{release_path}/public/last_update.txt"
 		run "ln -nfs #{shared_path}/system/files/issueresolution.wmv #{release_path}/public/issueresolution.wmv"
+		
 		run "ln -nfs #{shared_path}/system/files #{release_path}/public/files"
+		run "ln -nfs #{shared_path}/system/maps #{release_path}/public/maps"
 	end
 end
 
